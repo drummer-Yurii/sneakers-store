a9b1b787dc0259de7c6532a01b3aef5f4b46c99b
Yurii Rybachok <drummer2009@ukr.net>
2024-03-29 18:29:37 +0200

    create card & cardList components

---
+++ src/components/CardList.vue
@@ -0,0 +1,20 @@
+<script setup>
+import Card from './Card.vue'
+
+const onClickAdd = () => {
+  alert('111')
+}
+</script>
+
+<template>
+  <div class="grid grid-cols-4 gap-5">
+    <Card
+      title="Men's Sneakers Nike Blazer Suede"
+      imageUrl="/sneakers/sneakers-1.jpg"
+      :price="1205"
+      :isAdded="true"
+      :isFavourite="true"
+      :onClickAdd="onClickAdd"
+    />
+  </div>
+</template>
