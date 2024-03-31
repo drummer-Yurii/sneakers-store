0a2241c24eae1ff6a422647eb522be7924a15fc8
Yurii Rybachok <drummer2009@ukr.net>
2024-03-31 17:22:42 +0300

    create infoBlock component & show empty cart

--- src/components/Drawer.vue
+++ src/components/Drawer.vue
@@ -1,6 +1,7 @@
 <script setup>
 import DrawerHead from '@/components/DrawerHead.vue'
 import CartItemList from '@/components/CartItemList.vue'
+import InfoBlock from '@/components/InfoBlock.vue'
 
 const emit = defineEmits(['createOrder'])
 
@@ -15,26 +16,35 @@ defineProps({
   <div class="fixed top-0 left-0 h-full w-full bg-black z-10 opacity-70"></div>
   <div class="bg-white w-96 h-full fixed right-0 top-0 z-20 p-8">
     <DrawerHead />
-    <CartItemList />
+    <div v-if="!totalPrice" class="flex h-full items-center">
+      <InfoBlock
+        title="Cart empty"
+        description="Add at least one pair of sneakers to order."
+        imageUrl="/package-icon.png"
+      />
+    </div>
+    <div v-else>
+      <CartItemList />
 
-    <div class="flex flex-col gap-4 mt-7">
-      <div class="flex gap-2">
-        <span>Total:</span>
-        <div class="flex-1 border-b border-dashed"></div>
-        <b>{{ totalPrice }} UAH.</b>
+      <div class="flex flex-col gap-4 mt-7">
+        <div class="flex gap-2">
+          <span>Total:</span>
+          <div class="flex-1 border-b border-dashed"></div>
+          <b>{{ totalPrice }} UAH.</b>
+        </div>
+        <div class="flex gap-2">
+          <span>Tax 5%:</span>
+          <div class="flex-1 border-b border-dashed"></div>
+          <b>{{ vatPrice }} UAH.</b>
+        </div>
+        <button
+          :disabled="buttonDisabled"
+          @click="() => emit('createOrder')"
+          class="mt-4 transition bg-lime-500 w-full rounded-xl py-3 text-white disabled:bg-slate-400 hover:bg-lime-600 active:bg-lime-700 cursor-pointer"
+        >
+          Checkout
+        </button>
       </div>
-      <div class="flex gap-2">
-        <span>Tax 5%:</span>
-        <div class="flex-1 border-b border-dashed"></div>
-        <b>{{ vatPrice }} UAH.</b>
-      </div>
-      <button
-        :disabled="buttonDisabled"
-        @click="() => emit('createOrder')"
-        class="mt-4 transition bg-lime-500 w-full rounded-xl py-3 text-white disabled:bg-slate-400 hover:bg-lime-600 active:bg-lime-700 cursor-pointer"
-      >
-        Checkout
-      </button>
     </div>
   </div>
 </template>
