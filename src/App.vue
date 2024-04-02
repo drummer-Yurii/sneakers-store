6ec7af03056ae84698118ffe6eb157d74a073aa6
Yurii Rybachok <drummer2009@ukr.net>
2024-04-02 16:14:14 +0300

    refactoring cart

--- src/App.vue
+++ src/App.vue
@@ -1,22 +1,16 @@
 <script setup>
 import { computed, provide, ref, watch } from 'vue'
-import axios from 'axios'
 
 import Header from '@/components/Header.vue'
 import Drawer from '@/components/Drawer.vue'
 
 const cart = ref([])
-const isCreatingOrder = ref(false)
 
 const drawerOpen = ref(false)
 
 const totalPrice = computed(() => cart.value.reduce((acc, item) => acc + item.price, 0))
 
 const vatPrice = computed(() => Math.round((totalPrice.value * 5) / 100))
-
-const cartIsEmpty = computed(() => cart.value.length === 0)
-
-const cartButtonDisabled = computed(() => isCreatingOrder.value || cartIsEmpty.value)
 
 const closeDrawer = () => {
   drawerOpen.value = false
@@ -34,22 +28,6 @@ const addToCart = (item) => {
 const removeFromCart = (item) => {
   cart.value.splice(cart.value.indexOf(item), 1)
   item.isAdded = false
-}
-
-const createOrder = async () => {
-  try {
-    isCreatingOrder.value = true
-    const { data } = await axios.post(`https://c1871b3e151dd384.mokky.dev/orders`, {
-      items: cart.value,
-      totalPrice: totalPrice.value
-    })
-    cart.value = []
-    return data
-  } catch (error) {
-    console.log(error)
-  } finally {
-    isCreatingOrder.value = false
-  }
 }
 
 watch(
@@ -70,13 +48,7 @@ provide('cart', {
 </script>
 
 <template>
-  <Drawer
-    v-if="drawerOpen"
-    :total-price="totalPrice"
-    :vat-price="vatPrice"
-    @create-order="createOrder"
-    :button-disabled="cartButtonDisabled"
-  />
+  <Drawer v-if="drawerOpen" :total-price="totalPrice" :vat-price="vatPrice" />
   <div class="bg-white w-4/5 m-auto rounded-xl shadow-xl mt-14">
     <Header :total-price="totalPrice" @open-drawer="openDrawer" />
 
--- src/components/Drawer.vue
+++ src/components/Drawer.vue
@@ -1,15 +1,39 @@
 <script setup>
+import { ref, computed, inject } from 'vue'
+import axios from 'axios'
+
 import DrawerHead from '@/components/DrawerHead.vue'
 import CartItemList from '@/components/CartItemList.vue'
 import InfoBlock from '@/components/InfoBlock.vue'
 
-const emit = defineEmits(['createOrder'])
-
-defineProps({
+const props = defineProps({
   totalPrice: Number,
-  vatPrice: Number,
-  buttonDisabled: Boolean
+  vatPrice: Number
 })
+
+const { cart, closeDrawer } = inject('cart')
+
+const isCreating = ref(false)
+
+const createOrder = async () => {
+  try {
+    isCreating.value = true
+    const { data } = await axios.post(`https://c1871b3e151dd384.mokky.dev/orders`, {
+      items: cart.value,
+      totalPrice: props.totalPrice.value
+    })
+    cart.value = []
+    return data
+  } catch (error) {
+    console.log(error)
+  } finally {
+    isCreating.value = false
+  }
+}
+
+const cartIsEmpty = computed(() => cart.value.length === 0)
+
+const buttonDisabled = computed(() => isCreating.value || cartIsEmpty.value)
 </script>
 
 <template>
@@ -39,7 +63,7 @@ defineProps({
         </div>
         <button
           :disabled="buttonDisabled"
-          @click="() => emit('createOrder')"
+          @click="createOrder"
           class="mt-4 transition bg-lime-500 w-full rounded-xl py-3 text-white disabled:bg-slate-400 hover:bg-lime-600 active:bg-lime-700 cursor-pointer"
         >
           Checkout
